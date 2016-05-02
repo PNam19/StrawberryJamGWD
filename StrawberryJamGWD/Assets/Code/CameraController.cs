0161313c5e055c4939f01d2c5a885cfc5ad9f751
Erik Holbech <eholbech@gmail.com>
2016-05-02 15:16:29 +0200

    Implemented camera Controller
    Camera now follows the player according to variables;
    Margin: How far the player can move from the centre of the screen before
    the camera moves
    Smoothing: How aggressively the camera follows the player

---
+++ StrawberryJamGWD/Assets/Code/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform Player;
+
+    public float 
+        Margin,
+        Smooth;
+
+    public void Update()
+    {
+        var x = transform.position.x;
+        var y = Player.position.y+0.5f;
+        if (Mathf.Abs(x - Player.position.x) > Margin)
+            x = Mathf.Lerp(x, Player.position.x, Smooth * Time.deltaTime);
+        transform.position = new Vector3(x, y, transform.position.z);
+    }
+}
