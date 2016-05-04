a148f5b7bc48a4979a46ef7b7bba8d24f1e2945e
Erik Holbech <eholbech@gmail.com>
2016-05-04 14:14:55 +0200

    New assets, no more blurry sprites and smoke

---
+++ StrawberryJamGWD/Assets/Code/ParticleSorter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSorter : MonoBehaviour {
+
+    public string Layer = "Particles";
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = Layer;
+    }
+}
