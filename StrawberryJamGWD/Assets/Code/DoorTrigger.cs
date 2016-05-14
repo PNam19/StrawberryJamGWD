401060fd7a9c3817d0639854a3910d5c635f690d
Erik Holbech <eholbech@gmail.com>
2016-05-14 13:48:31 +0200

    Created inside level and changing between levels
    Still spawn at the side of the island when returning to the start scene

---
+++ StrawberryJamGWD/Assets/Code/DoorTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrigger : MonoBehaviour
+{
+
+    private bool colliding = false;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            colliding = true;
+            Debug.Log("true");
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            colliding = false;
+            Debug.Log("false");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) && colliding)
+        {
+            Application.LoadLevel(1);
+        }
+    }
+}
